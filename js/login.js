c4b3d18b868816a640763bc3fe26dfd52a0d7e61
chanicoh <chanicoh@g.jct.ac.il>
2024-03-05 16:32:19 +0200

    spa finish login signup

--- js/login.js
+++ js/login.js
@@ -20,7 +20,7 @@ const app = {
     nav: function(ev){
         ev.preventDefault();
         let currentPage = ev.target.getAttribute('data-target');
-        document.querySelector('.form-box.active').classList.remove('active');
+        document.querySelector('.active').classList.remove('active');
         document.getElementById(currentPage).classList.add('active');
         history.pushState({}, currentPage, `#${currentPage}`);
         document.getElementById(currentPage).dispatchEvent(app.show);
@@ -36,7 +36,7 @@ const app = {
     poppin: function(ev){
         console.log(location.hash, 'popstate event');
         let hash = location.hash.replace('#' ,'');
-        document.querySelector('.form-box.active').classList.remove('active');
+        document.querySelector('.active').classList.remove('active');
         document.getElementById(hash).classList.add('active');
         document.getElementById(hash).dispatchEvent(app.show);
     }
