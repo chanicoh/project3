5d009b96813a137b5b7939ba08627466c6dc9386
chanicoh <chanicoh@g.jct.ac.il>
2024-03-05 14:22:55 +0200

    spa

---
+++ js/login.js
@@ -0,0 +1,45 @@
+
+const app = {
+    pages: [],
+    show: new Event('show'),
+    init: function(){
+        app.pages = document.querySelectorAll('.form-box');
+        app.pages.forEach((formBox)=>{
+            formBox.addEventListener('show', app.pageShown);
+        })
+
+        document.querySelectorAll('.register-link').forEach((link)=>{
+            link.addEventListener('click', app.nav);
+        })
+        document.querySelectorAll('.login-link').forEach((link)=>{
+            link.addEventListener('click', app.nav);
+        })
+        history.replaceState({}, 'Login', '#login');
+        window.addEventListener('popstate', app.poppin);
+    },
+    nav: function(ev){
+        ev.preventDefault();
+        let currentPage = ev.target.getAttribute('data-target');
+        document.querySelector('.form-box.active').classList.remove('active');
+        document.getElementById(currentPage).classList.add('active');
+        history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(currentPage).dispatchEvent(app.show);
+    },
+    pageShown: function(ev){
+        console.log('Page', ev.target.id, 'just shown');
+        let h2 = ev.target.querySelector('h2');
+        h2.classList.add('big')
+        setTimeout((h)=>{
+            h.classList.remove('big');
+        }, 1200, h2);
+    },
+    poppin: function(ev){
+        console.log(location.hash, 'popstate event');
+        let hash = location.hash.replace('#' ,'');
+        document.querySelector('.form-box.active').classList.remove('active');
+        document.getElementById(hash).classList.add('active');
+        document.getElementById(hash).dispatchEvent(app.show);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
